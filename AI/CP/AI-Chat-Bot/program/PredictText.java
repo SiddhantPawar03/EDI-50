b391715d279b6113739bc3f40b509e0e11276256
SiddhantPawar03 <siddhantpawar1203@gmail.com>
2022-11-13 17:40:39 +0530

    Update

--- AI/CP/AI-Chat-Bot/program/PredictText.java
+++ AI/CP/AI-Chat-Bot/program/PredictText.java
@@ -17,22 +17,52 @@ public class PredictText extends JFrame implements KeyListener
 	
 	String chatBot[][] = {
 			//Greeting
-			{"hi", "hello", "hola", "helo", "ola", "oy", "ey", "hey"},
-			{"Hello there! Biggol is 24/7 online for you","Hello dear!", "Yes, say"},
-			//question 1: 
-			{"how are you doing", "how r u doing", "hw r u doin", "how are you", "hw are you", "how r you", "how are u", "hw r you", "hw are u","how r u", "hw r u"},
-			{"Good","Fine","Amazing","Great","Well! Everything's good!"},
-			//question 2
-			{"what are you doing", "wt are you doing", "what r you doing", "what are u doing", "what r u doing", "wt r u doing", "wt r u doin"},
-			{"Dude! Coing is life", "Sleeping like a panda!", "Having my caffiene, You want some?", "Learning new tech like a nerd", "Being Geeky", "Answering you, blind fella!","Being witty to cut your cum inside"},
-			//Negation 1
-			{"yes", "ya", "yeah" , "yea", "ha","ya ya", "okay", "ok", "okey", "oki", "yay", "yo", "yaay"},
-			{"No", "Never", "No, You can't", "Na", "Nay", "No No", "Ok Fine", "K", "Hmm", "Hm"},
-			//Negation 1
-			{"no", "never", "no, you can't", "na", "nay", "no no", "ok fine", "k", "hmm", "hm"},
-			{"Yes", "Ya", "Yeah" , "Of course yes", "Yea", "Ha","Ya Ya", "Okay", "Ok", "Okey", "Oki", "Yay", "Yo", "Yaay"},
+			{"Hi", "How are you", "Is anyone there?", "Hello", "Good day", "Whats up", "how are ya", "heyy", "whatsup"},
+			{"Hello!", "Good to see you again!", "Hi there, how can I help?"},
+			//Goodbye
+			{"cya", "see you", "bye bye", "See you later", "Goodbye", "I am Leaving", "Bye", "Have a Good day", "talk to you later", "tyyl", "i got to go", "gtg"},
+			{"Sad to see you go :(", "Talk to you later", "Goodbye!","Come back soon"},
+			//Creators
+			{"what is the name of your developers", "what is the name of your creators", "what is the name of the developers", "what is the name of the creators", "who created you", "your developers", "your creators", "who are your developers", "developers", "you are made by", "you are made by whom", "who created you", "who create you", "creators", "who made you", "who designed you"},
+			{"I was developed by team of smart people named Mandar, Maruti, Pavan, Siddhant under the guidance of GDB :)"},
+			//Name
+			{"name", "your name", "do you have a name", "what are you called", "what is your name", "what should I call you", "whats your name?", "what are you", "who are you", "who is this", "what am i chatting to", "who am i taking to", "what are you"},
+			{"You can call me College Mitra.","I'm College Mitra","I'm College Mitra aka CMGDB's Chatbot."},
+			//hours 
+			{"timing of college", "what is college timing", "working days", "when are you guys open", "what are your hours", "hours of operation", "when is the college open", "college timing", "what about college timing", "is college open on saturday", "tell something about college timing", "what is the college  hours", "when should i come to college", "when should i attend college", "what is my college time", "college timing", "timing college"},
+			{"College is open 9am-5pm Monday-Friday!"},
+			//number
+			{"more info", "contact info", "how to contact college", "college telephone number", "college number", "What is your contact no", "Contact number?", "how to call you", "College phone no?", "how can i contact you", "Can i get your phone number", "how can i call you", "phone number", "phone no", "call"},
+			{"You can contact at +91-011-25318157"},
+			//course
+			{"list of courses", "list of courses offered", "list of courses offered in VIT", "what are the courses offered in your college?", "courses?", "courses offered", "courses offered in VIT", "courses you offer", "branches?", "courses available at VIT?", "branches available at VIT?", "what are the courses in VIT?", "what are branches in VIT?", "what are courses in VIT?", "branches available in VIT?", "can you tell me the courses available in VIT?", "can you tell me the branches available in VIT?", "Civil engineering?", "civil", "it", "IT"},
+			{"VIT offers Computer Engineering, Information Technology, AI & DS, Instrumentation Engineering, Civil Engineering and Mechanical Engineering."},
+			//fees
+			{"information about fee", "information on fee", "tell me the fee", "college fee", "fee per semester", "what is the fee of each semester", "what is the fees of each year", "what is fee", "what is the fees", "how much is the fees", "fees for first year", "fees", "about the fees", "tell me something about the fees"},
+			{"For Fee detail visit <a target=\"_blank\" href=\"https://www.vit.edu/admissions/fees-structure\"> here</a>"},
+			//location
+			{"where is the college located", "college is located at", "where is college", "where is college located", "address of college", "how to reach college", "college location", "college address", "wheres the college", "how can I reach college", "whats is the college address", "what is the address of college", "address", "location"},
+			{"666, Upper Indiranagar, Bibwewadi,Pune, Maharashtra, INDIA - 411 037."},
+			//hostel
+			{"hostel facility", "hostel servive", "hostel location", "hostel address", "hostel facilities", "hostel fees", "Does college provide hostel", "Is there any hostel", "Where is hostel", "do you have hostel", "do you guys have hostel", "hostel", "hostel capacity", "what is the hostel fee", "how to get in hostel", "what is the hostel address", "how far is hostel from college", "hostel college distance", "where is the hostel", "how big is the hostel", "distance between college and hostel", "distance between hostel and college"},
+			{"666, Upper Indiranagar, Bibwewadi,Pune, Maharashtra, INDIA - 411 037.", "Please find rooms else ..."},
+			//event
+			{"events organised", "list of events", "list of events organised in college", "list of events conducted in college", "What events are conducted in college", "Are there any event held at college", "Events?", "functions", "what are the events", "tell me about events", "what about events"},
+			{"Please follow events via EPEC VIT Pune Social Media accounts."},
+			//canteen
+			{"food facilities", "canteen facilities", "canteen facility", "is there any canteen", "Is there a cafetaria in college", "Does college have canteen", "Where is canteen", "where is cafetaria", "canteen", "Cafetaria"},
+			{"There are two major canteen facilities one being old one while other new one and NPB also in campus premises as well."},
+			//placement
+			{"What is college placement", "Which companies visit in college", "What is average package", "companies visit", "package", "placement", "recruitment", "companies"},
+			{"Few placed in Microsoft forgetting friends :("},
+			//college intake
+			{"max number of students", "number of seats per branch", "number of seats in each branch", "maximum number of seats", "maximum students intake", "What is college intake", "how many stundent are taken in each branch", "seat allotment", "seats"},
+			{"For CS, IT, 250 approx each branch, Civil and Mechanical 60 per branch", "Paisa deo admission leo."},
+			//placement
+			{"What is college placement", "Which companies visit in college", "What is average package", "companies visit", "package", "placement", "recruitment", "companies"},
+			{"Few placed in Microsoft forgetting friends :("},
 			//default
-			{"you need sleep", "idk say else","stupid answer","la la la la" ,"Shut up nood!", "Bullshit!"}
+			{"Unable to understand."}
 			
 	};
 	
